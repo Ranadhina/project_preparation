05ec30d9cbd7088008b3532d0dd94cd6da2a7937
Ranadhina <ranadhina.reddy@gmail.com>
2025-11-06 10:47:34 +0530

    Revert "Revert "Revert "Initial Commit -added all services to project_preparation"""
    This reverts commit c2b9b467ee74fb86a7bcc927ec0817d949e5ca21.

--- Subscription/src/main/java/com/practice/Subscription/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.practice.Subscription.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
--- customer-service/src/main/java/com/example/customer_service/repo/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.customer_service.repo;
-import com.example.customer_service.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-    boolean existsByEmail(String email);
-}
